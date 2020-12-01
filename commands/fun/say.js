855a369ce6432ce96c388c7046dfbfd0cd917c64
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2020-12-01 19:34:00 +0100

    Update say.js

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -4,6 +4,7 @@ module.exports.run = (message, args) => {
     let Saymessage = args.join(" ")
     if(!Saymessage) return message.reply("Tu dois précisé ton message!")
     message.channel.send(Saymessage)
+    message.delete()
 }
 
 module.exports.help = {
