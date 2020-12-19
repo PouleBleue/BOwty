26db35bfa22346d7c03bcfcda8f68c83b0f9fabd
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2020-12-19 13:26:47 +0100

    Update 8ball.js

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -7,7 +7,7 @@ module.exports.run = (message, args) => {
 
         if(!jérémy) return message.reply("Précise ta question !") 
         else {
-            let K = [":8ball: J'ai pas envie de répondre à cela"," :8ball: Oui", ":8ball: Non", ":8ball: Peut-être", ":8ball: Je ne pense pas", ":8ball: nan",":8ball: Sûrement", ":8ball: Selon mes sources je crois que non", ":8ball: Absolument", "Je ne sais pas", "Aucune idée", "Oui", "Non"]
+            let K = [":8ball: Oui", ":8ball: Non", ":8ball: Peut-être", ":8ball: Je ne pense pas", ":8ball: nan",":8ball: Sûrement", ":8ball: Selon mes sources je crois que non", ":8ball: Absolument", "Je ne sais pas", "Aucune idée", "Oui", "Non", ":8ball: J'ai pas envie de répondre à cela"]
             let MDR = K[Math.floor(Math.random() * K.length)];
 
             let embed = new Discord.MessageEmbed()
