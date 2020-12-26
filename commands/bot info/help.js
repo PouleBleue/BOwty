438f0a1053afaacb0e7140bf5729a8978563db1d
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2020-12-26 22:13:43 +0100

    Update help.js

--- commands/bot info/help.js
+++ commands/bot info/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = (message, args) => {
-    message.channel.send("**__Liste des commandes :__** ```\n-b!demute \n-b!tmute \n-b!trigger \n-b!mute \n-b!dm \n-b!help : affiche ce message \n-b!warn \n-b!botinfo \n-b!qi \n-b!work \n-b!balance \n-b!daily  \n-b!dog \n-b!say \n-b!userinfo \n-b!hug \n-b!slap \n-b!cat \n-b!pfc (Pierre, Feuille, Ciseaux) \n-b!credit \n-b!avatar \n-b!ping : Commande de test \n-b!clear : Le bot efface les messages \n-b!8ball \n-b!kick \n-b!ban \n-b!meme```")
+    message.channel.send("**__Liste des commandes :__** ```\n-b!join \n-b!leave \n-b!demute \n-b!tmute \n-b!trigger \n-b!mute \n-b!dm \n-b!help : affiche ce message \n-b!warn \n-b!botinfo \n-b!qi \n-b!work \n-b!balance \n-b!daily  \n-b!dog \n-b!say \n-b!userinfo \n-b!hug \n-b!slap \n-b!cat \n-b!pfc (Pierre, Feuille, Ciseaux) \n-b!credit \n-b!avatar \n-b!ping : Commande de test \n-b!clear : Le bot efface les messages \n-b!8ball \n-b!kick \n-b!ban \n-b!meme```")
 
 }
 
