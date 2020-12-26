b8dacafdfd6ed8f0cdf0507a63205fdc240fae84
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2020-12-26 21:56:10 +0100

    Update random.js

--- commands/fun/random.js
+++ commands/fun/random.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (message, args) => {
     let random = Math.floor(Math.random() * 9);
-    message.channel.send(`**Le nombre aléatoire est ${random}`)
+    message.channel.send(`**Le nombre aléatoire est ${random}**`)
 }
 
 module.exports.help = {
