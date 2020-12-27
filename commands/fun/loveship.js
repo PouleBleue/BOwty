a880791499d6dc80a56f370c4335442c05ad7f3d
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2020-12-27 02:57:53 +0100

    Update loveship.js

--- commands/fun/loveship.js
+++ commands/fun/loveship.js
@@ -2,9 +2,7 @@ module.exports.run = (message, args) => {
     let person =  message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
 
     if (!person || message.author.id === person.id) {
-        person = message.guild.members
-            .filter(m => m.id !== message.author.id)
-            .random();
+        person = message.guild.members.cache.filter(m => m.id !== message.author.id).random();
     }
 
     const love = Math.random() * 100;
