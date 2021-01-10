793601e9466ed674bbb7ac3233804ebf08daca1d
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-01-10 13:25:20 +0100

    Update userinfo.js

--- commands/bot info/userinfo.js
+++ commands/bot info/userinfo.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const moment = require("moment");
 
 module.exports.run = (message, args) => {
     let mentionMember = message.mentions.members.first();
@@ -11,8 +12,8 @@ module.exports.run = (message, args) => {
     .setColor("#0000FF")
     .addField("Nom d'utilisateur:", `${mentionUser.username}`)
     .addField("ID:", `${mentionUser.id}`)
-    .addField("Création du compte:", `${mentionUser.createdAt}`)
-    .addField("A rejoins le serveur:", `${mentionMember.joinedAt}`)
+    .addField("Création du compte:", `${moment(mentionUser.createdAt).format("DD/MM/YYYY")}`)
+    .addField("A rejoins le serveur:", `${moment(mentionMember.joinedAt).format("DD/MM/YYYY")}`)
     .addField("Statut de l'utilisateur:", `${mentionUser.presence.status}`)
     message.channel.send(embed)
 }
