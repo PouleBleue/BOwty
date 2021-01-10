d627d46f841d621a5e29df07b1801325f3da5a17
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-01-10 13:24:00 +0100

    Update botinfo.js

--- commands/bot info/botinfo.js
+++ commands/bot info/botinfo.js
@@ -8,7 +8,7 @@ module.exports.run = (message, args) => {
     .addField("Bot crée :", `Le 18 octobre 2020`)
     .addField("Nombre total de personnes :", message.client.users.cache.size)
     .addField("Nombre total de serveur :", message.client.guilds.cache.size)
-    .addField("Version bêta :", `Version : 0.1.2.0`)
+    .addField("Version bêta :", `Version : 0.1.6.0`)
     .setTimestamp()
 message.channel.send(binfo_embed)
 }
