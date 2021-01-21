4f8d722d06e1311a59c41684d19425656492f957
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-01-21 17:45:32 +0100

    Update say.js

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -3,8 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = (message, args) => {
     let Saymessage = args.join(" ")
     if(!Saymessage) return message.reply("Tu dois précisé ton message!")
-    message.channel.send(Saymessage)
-    message.delete()
+    message.channel.send(`**Le message de ${message.author.username} est ${Saymessage}**`)
 }
 
 module.exports.help = {
