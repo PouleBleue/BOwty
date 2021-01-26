8aa5747dfd587993e5a44a11d699a0df78d29e04
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-01-26 20:13:06 +0100

    Update qi.js

--- commands/fun/qi.js
+++ commands/fun/qi.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (message, args) => {
     let user = message.mentions.users.first() || message.author
     if(!user) return message.channel.send("Tu as oublié de mentionné quelqu'un")
-    let qi = Math.floor(Math.random() * 500) + 1;
+    let qi = Math.floor(Math.random() * 200) + 1;
     message.channel.send(`**${user} a ${qi} de QI**`)
 }
 
