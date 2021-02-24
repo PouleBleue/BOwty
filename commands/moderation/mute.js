342d2700a288cffe6397b091cf97caf45544377b
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-02-24 09:40:25 +0100

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,35 +1,29 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (message, args) => {
-    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+     let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
     if(!tomute) return message.reply("Je n'ai pas trouvé d'utilisateur !");
     let user = message.mentions.users.first();
-    let raison = args.join(" ").slice(22);
+    let raison = args[1];
     if (user === message.author) return message.channel.send("Tu ne peux pas te mute toi même ! ")
     if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Il te manque des permissions");
-    let muterole = message.guild.roles.cache.find(role => role.name === "muted");
+    let muterole = message.guild.roles.cache.find(role => role.name === "Muted");
     if(!raison) return message.channel.send("Tu as oublié de mettre une raison !")
     tomute.roles.add(muterole.id);
-    message.reply(`<@${tomute.id}> a été mute!`);
-    user.send(`Tu as été mute !`)
+    const pdp = user.displayAvatarURL()
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Mute")
+    .setColor("RANDOM")
+    .setThumbnail(pdp)
+    .addField("Utilisateur :", `<@${tomute.id}>`)
+    .addField("Mute par :", `${message.author.username}`)
+    .addField("Raison :", `${raison}`)
+    message.channel.send(embed)
 
     if(!muterole){
-      try{
-        muterole = await message.guild.roles.create({
-          name: "muted",
-          color: "#000000",
-          permissions:[]
-        })
-        message.guild.channels.cache.forEach(async (channel, id) => {
-          await channel.overwritePermissions(muterole, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false
-          });
-        });
-      }catch(e){
-        console.log(e.stack);
+      message.channel.send("Merci de crée un rôle `Muted`(Le bot créera le rôle automatiquement, cette fonctionnalité sera ajouté dans une future mise à jour)")
       }
-    }
 }
 
 
