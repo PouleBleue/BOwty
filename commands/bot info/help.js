8f664e0a7b8a466c0456c599bfc177cbd39af9e4
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-02-24 09:47:57 +0100

    Update help.js

--- commands/bot info/help.js
+++ commands/bot info/help.js
@@ -3,8 +3,8 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports.run = (message, args) => {
     const embed = new MessageEmbed()
-    .setTitle("Liste des commandes: (prefix : n!)")
-    .addField("Modération:", "`Ban`,`Kick`, `clear`, `mute`, `nuke`, `tmute`, `warn`, `dm`")
+    .setTitle("Liste des commandes: (prefix : b!)")
+    .addField("Modération:", "`Ban`,`Kick`, `clear`, `mute`, `nuke`, `tmute`, `dm`")
     .addField("Economie (En bêta) ", "`daily`, `balance`, `work`" )
     .addField("Fun:", "`8ball`, `cat`, `dog`, `fox`, `hug`, `meme`, `pfc`, `qi`, `random`, `say`, `slap`, `trigger`")
     .addField("Bot info :", "`avatar`, `botinfo`, `help`, `ping`, `serverinfo`, `userinfo`")
