dba355ec374fa081ce15761dffa947b3c15e91dd
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-05-02 11:23:32 +0200

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,13 +1,16 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (message, args) => {
-     let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+  let muterole = message.guild.roles.cache.find(role => role.name === "Muted");
+  if(!muterole){
+    return message.channel.send("Le rôle mute est introuvable, veuillez utiliser la commande `b!configmute`")
+  };
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
     if(!tomute) return message.reply("Je n'ai pas trouvé d'utilisateur !");
     let user = message.mentions.users.first();
     let raison = args[1];
     if (user === message.author) return message.channel.send("Tu ne peux pas te mute toi même ! ")
     if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Il te manque des permissions");
-    let muterole = message.guild.roles.cache.find(role => role.name === "Muted");
     if(!raison) return message.channel.send("Tu as oublié de mettre une raison !")
     tomute.roles.add(muterole.id);
     const pdp = user.displayAvatarURL()
@@ -21,12 +24,7 @@ module.exports.run = async (message, args) => {
     .addField("Raison :", `${raison}`)
     message.channel.send(embed)
 
-    if(!muterole){
-      message.channel.send("Merci de crée un rôle `Muted`(Le bot créera le rôle automatiquement, cette fonctionnalité sera ajouté dans une future mise à jour)")
-      }
-}
-
-
+    }
 module.exports.help = {
     name: "mute",
     description: "owo"
