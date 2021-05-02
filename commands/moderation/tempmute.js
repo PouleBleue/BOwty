9629a795c7e89390cd5a04776498e69f567971dc
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-05-02 11:24:10 +0200

    Update tempmute.js

--- commands/moderation/tempmute.js
+++ commands/moderation/tempmute.js
@@ -1,18 +1,19 @@
 const Discord = require("discord.js");
+const { set } = require("mongoose");
 const ms = require("ms");
 
 module.exports.run = async (message, args) => {
-       let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+  let muterole = message.guild.roles.cache.find(role => role.name === "Muted");
+  if(!muterole){
+    return message.channel.send("Le rôle mute est introuvable, veuillez utiliser la commande `b!configmute`")
+  };
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
     if(!tomute) return message.reply("Je n'ai pas trouvé d'utilisateur !");
+    let guild = message.guild
     let user = message.mentions.users.first();
     if (user === message.author) return message.channel.send("Tu ne peux pas te mute toi même ! ")
     if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Il te manque des permissions");
-    let muterole = message.guild.roles.cache.find(role => role.name === "Muted");
     let raison = args[2];
-
-    if(!muterole){
-      message.channel.send("Merci de crée un rôle `Muted`(Le bot créera le rôle automatiquement, cette fonctionnalité sera ajouté dans une future mise à jour)")
-    }
     let mutetime = args[1];
     if(!mutetime) return message.reply("Il faut que tu dises le temps exact pendant lequel l'utilisateur choisi sera mute !");
     if(!raison) return message.channel.send("Merci de mettre une raison !");
@@ -32,13 +33,13 @@ module.exports.run = async (message, args) => {
 
 
 
-    user.send(`Tu as été mute pendant ${ms(ms(mutetime))}`)
+    user.send(`Tu as été mute du serveur portant le nom de ${guild.name} pendant ${ms(ms(mutetime))}`)
   
     setTimeout(function(){
       tomute.roles.remove(muterole.id);
       message.channel.send(`<@${tomute.id}> a été unmute!`);
     }, ms(mutetime));
-}
+  }
 module.exports.help = {
     name: "tmute",
     description: "owo"
