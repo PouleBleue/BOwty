12f38c96d6a190bf769bd6a4620b6edea9a3d1f1
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-05-03 20:55:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ ws: { properties: { $browser: "Discord iOS" }} });
+const client = new Discord.Client();
 const { prefix, token } = require("./config.json")
 const { readdirSync, fs  } = require('fs');
-
 
 client.commands = new Discord.Collection();
 
@@ -20,13 +19,26 @@ const loadCommands = (dir = "./commands/") => {
 
 loadCommands();
 
+client.on("guildMemberAdd", member => {
+    const welcome = member.guild.channels.cache.find(welcome => welcome.name === "〢・😎・》bienvenue")
+    if(!welcome) return;
+    member.send(`Salut, ${member} sois le Bienvenue dans le serveur de PouleBleue qui est mon créateur, je suis un bot en developpement`)
+    welcome.send(`Bienvenue ${member} dans le Royaume des bob-omb!`)
+});
+
+client.on("guildMemberRemove", member => {
+    const welcome = member.guild.channels.cache.find(welcome => welcome.name === "〢・😎・》bienvenue")
+    if(!welcome) return;
+    member.send("Rip, Tu as quitté le serveur, mais pourquoi ?!")
+    welcome.send(`${member} a quitté le serveur, c'est beaucoup trop triste !`)
+    
 });
 
 client.on('ready', () => {
     console.log('Je dois dire : Olivia est conne !');
     const number = client.users.cache.size
     const server = client.guilds.cache.size
-    client.user.setActivity(`b0.1.9.2 || ${server} serveurs et à ${number} utilisateurs ||  b!help`, { type: "PLAYING" })
+    client.user.setActivity(`b0.1.8.0 || ${server} serveurs et à ${number} utilisateurs ||  b!help`, { type: "PLAYING" })
 });
 
  
