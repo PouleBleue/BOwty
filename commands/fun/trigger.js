2c308a73d7c9518b3c5e1f29b42fd67538d028f5
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-05-27 22:02:38 +0200

    Update trigger.js

--- commands/fun/trigger.js
+++ commands/fun/trigger.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 
 module.exports.run = async (message, args) => {
     let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-    let triggered = await canvacord.Canvas.wasted((user.displayAvatarURL({ format: "png", dynamic: false, size: 4096})));
+    let triggered = await canvacord.Canvas.trigger((user.displayAvatarURL({ format: "png", dynamic: false, size: 4096})));
     let attachment = new MessageAttachment(triggered, "triggered.gif");
     return message.channel.send(attachment);
 }
