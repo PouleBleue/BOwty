3509268ae545e9645c8ea785f5dd4c4f41fbc710
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-06-02 22:20:03 +0200

    Update qi.js

--- commands/fun/qi.js
+++ commands/fun/qi.js
@@ -3,8 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (message, args) => {
     let buser = message.mentions.users.first()
     let guildMembers = await message.guild.members.fetch() 
-    let user =  message.mentions.users.first() || args[1]  
-    if(!user) return message.channel.send("owo")
+    let user =  message.mentions.users.first() || args[1];  
     let qi = Math.floor(Math.random() * 250) + 1;
     message.channel.send(`**${user} a ${qi} de QI**`)
     }
