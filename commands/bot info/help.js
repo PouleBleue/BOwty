8b41b4a6c45e4e6ad378b0f3b283987d95d6ef3e
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-06-27 22:06:29 +0200

    Update help.js

--- commands/bot info/help.js
+++ commands/bot info/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (message, args) => {
     const owo = new Discord.MessageEmbed()
         .setTitle("Liste des commandes: (prefix : b!)")
         .addField("Modération:", "`Ban`,`Kick`, `clear`, `mute`, `nuke`, `tmute`, `dm`, `unban`")
-        .addField("Fun:", "`8ball`, `cry`, `wasted`, `crandom`, `cat`, `dog`, `fox`, `hug`, `meme`, `pfc`, `say`, `slap`, `trigger`, `rip`, `inverse`, `facepalm`, `emojify`, `baka`")
+        .addField("Fun:", "`8ball`, `cry`, `wasted`, `crandom`, `cat`, `dog`, `fox`, `hug`, `meme`, `say`, `slap`, `trigger`, `rip`, `inverse`, `facepalm`, `emojify`, `baka`")
         .addField("Bot info :", "`avatar`, `botinfo`, `help`, `ping`, `serverinfo`, `userinfo`, `roleinfo`")
         .setColor("#0000FF")
         message.channel.send(owo)
