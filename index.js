7db834dd2f52e609ff59cf2904cfd0c4cb3df931
PouleBleue <73319311+PouleBleue@users.noreply.github.com>
2021-12-11 21:08:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on('ready', () => {
     console.log('Je dois dire : Olivia est conne !');
     const number = client.users.cache.size
     const server = client.guilds.cache.size
-    client.user.setActivity(`g1.0.2 || ${server} serveurs et à ${number} utilisateurs ||  b!help`, { type: "PLAYING" })
+    client.user.setActivity(`g1.0.3 || ${server} serveurs et à ${number} utilisateurs ||  b!help`, { type: "PLAYING" })
 });
 
  
